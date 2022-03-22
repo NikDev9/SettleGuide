63411c994bee6e99d81f752e459419d803693d98
Nikita <nikita_desai@ymail.com>
2022-03-22 14:46:31 -0600

    deployment update

--- GuideApi/GuideApi/settings.py
+++ GuideApi/GuideApi/settings.py
@@ -162,10 +162,6 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [os.path.join(REAL_BASE_DIR, 'frontend', 'build', 'static')]
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'build/static')
-# ]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
